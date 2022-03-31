3fb576970ba1d7ca1bdba7e0b4dce99df660e8ee
guskoro <guskoro@gmail.com>
2022-03-31 11:32:26 +0700

    onboarding  process

--- SayangSemuanya/ViewController.swift
+++ SayangSemuanya/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //test 123
+        //test ui
         // Do any additional setup after loading the view.
     }
 
