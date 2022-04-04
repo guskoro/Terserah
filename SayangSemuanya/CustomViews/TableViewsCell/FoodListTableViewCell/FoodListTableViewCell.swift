833f2d53b61044cada518a9a34cc189332756b9e
agushery <hebatmandor@gmail.com>
2022-04-04 21:02:02 +0800

    Create TableViewCell for FoodList

---
+++ SayangSemuanya/CustomViews/TableViewsCell/FoodListTableViewCell/FoodListTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FoodListTableViewCell.swift
+//  SayangSemuanya
+//
+//  Created by Agus Hery on 04/04/22.
+//
+
+import UIKit
+
+class FoodListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var foodImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
