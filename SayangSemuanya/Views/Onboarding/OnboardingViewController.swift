445e9b42d7b1799e19118968b401e3394b0b49e4
guskoro <guskoro@gmail.com>
2022-04-06 05:35:25 +0700

    connecting ui to controller onboarding 1

---
+++ SayangSemuanya/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingViewController.swift
+//  SayangSemuanya
+//
+//  Created by Guskoro Pradipta on 06/04/22.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func nextButtonClicked(_ sender: UIButton) {
+    }
+}
